9339f7d3e9d73450c25beab4a0f4221c897a9bc7
Elliot Kang <kkang2097@gmail.com>
2023-07-01 00:30:32 -0700

    feed boilerplate, add content after API

---
+++ lib/views/feed.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class Feed extends StatefulWidget {
+
+  @override
+  _FeedState createState() => _FeedState();
+}
+
+
+class _FeedState extends State<Feed> {
+  //Add state helper functions
+  int incrementor = 0;
+  void state_temp(){
+    incrementor++;
+  }
+
+  //Return value, a widget
+  @override
+  Widget build(BuildContext context) {
+    return Text('Feeeed');
+  }
+
+}
