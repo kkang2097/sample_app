c25f4a8a9052a30396193b07294708d8a1e65529
Elliot Kang <kkang2097@gmail.com>
2023-07-29 04:12:52 -0700

    Update login_service.dart

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -16,6 +16,7 @@ bool verify_login(String id) {
 Future<Map<String, dynamic>> get_user_check<T>(String url, String db_api_pw, String username) async {
   print("STARTING HTTP REQUEST");
   print('$url/getuser?username=$username');
+  //We should send HTTP requests without headers, using headers is a bad practice.
   final res = await http.get(Uri.parse('$url/getuser').replace(queryParameters: {'username': username}));
 
   //Decode the response and put it as a Future, since this is an async function.
@@ -26,9 +27,10 @@ Future<Map<String, dynamic>> get_user_check<T>(String url, String db_api_pw, Str
 }
 
 
+
+
+
 //Main function to test DB connection...
-
-
 Future<void> main(List<String> args) async {
   var data = await get_user_check('http://localhost:8000', 'api_pw', 'user');
   print(data);
